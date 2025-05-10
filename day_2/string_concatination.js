208ed21f7a065c8e8ec11076616236db3f0f7167
[Myo Zayar Aung] <[myozayaraung705@gmail.com]>
2025-05-11 02:34:38 +0630

    test commit

--- day_2/string_concatination.js
+++ day_2/string_concatination.js
@@ -8,5 +8,3 @@ let fullname = firstname + lastname;
 let Template1 = "I am " + fullname + ". And i live in " + country ;
 
 let Template2 = `I am ${fullname}. And i live in ${country}. I am ${age}.`
-
-let myname = "myozayaraung";
